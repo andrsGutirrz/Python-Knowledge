7651c5d617316973f80443a718af7185c1fdcaf5
andrsGutirrz <andres.gutierrez.arcia@gmail.com>
2019-07-26 16:18:47 -0600

    model user

--- platzi-django-course/platzigram/posts/models.py
+++ platzi-django-course/platzigram/posts/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    """User model"""
+
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=101)
+
+    first_name = models.CharField(max_length=100)
+    las_name = models.CharField(max_length=100)
+
+    bio = models.TextField(blank=True)
+
+    birthdate = models.DateField(blank=True, null=True)
+
+    created = models.DateTimeField(auto_now_add=True) #includes hour
+    modified = models.DateTimeField(auto_now=True)
+
+    is_admin = models.BooleanField(default=False)
